4e4ed0017bd688202e54691100a704ebc877ce8e
DetaPranata <adetarakapranata123@gmail.com>
2026-06-05 03:32:01 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const comics = [
+  {
+    title: "Comic 1",
+    cover: "comics/comic-1/cover.jpg",
+    folder: "comics/comic-1",
+  },
+  {
+    title: "Comic 2",
+    cover: "comics/comic-2/cover.jpg",
+    folder: "comics/comic-2",
+  },
+];
+
+const list = document.getElementById("comic-list");
+
+comics.forEach((c) => {
+  const card = document.createElement("div");
+  card.className = "comic-card";
+
+  card.innerHTML = `
+    <img src="${c.cover}">
+    <h3>${c.title}</h3>
+  `;
+
+  card.onclick = () => {
+    window.location.href = `${c.folder}/index.html`;
+  };
+
+  list.appendChild(card);
+});

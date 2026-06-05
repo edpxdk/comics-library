0f2389e476044c49b8e464628a0b91b4d1e9d483
DetaPranata <adetarakapranata123@gmail.com>
2026-06-05 11:56:08 +0800

    update css

--- app.js
+++ app.js
@@ -9,16 +9,32 @@ const comics = [
     cover: "comics/comic-2/cover.jpg",
     folder: "comics/comic-2",
   },
+  {
+    title: "Comic 3",
+    cover: "comics/comic-3/cover.jpg",
+    folder: "comics/comic-3",
+  },
+  {
+    title: "Comic 4",
+    cover: "comics/comic-4/cover.jpg",
+    folder: "comics/comic-4",
+  },
+  {
+    title: "Comic 5",
+    cover: "comics/comic-5/cover.jpg",
+    folder: "comics/comic-5",
+  },
 ];
 
 const list = document.getElementById("comic-list");
 
 comics.forEach((c) => {
   const card = document.createElement("div");
+
   card.className = "comic-card";
 
   card.innerHTML = `
-    <img src="${c.cover}">
+    <img src="${c.cover}" alt="${c.title}">
     <h3>${c.title}</h3>
   `;
 
